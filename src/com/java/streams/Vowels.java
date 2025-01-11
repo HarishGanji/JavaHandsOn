2fa143e3a8bb751df3c87cf64fb4a949cfcf9adb
HarishGanji <harishganji7989@gmail.com>
2025-01-11 13:23:04 +0530

    find vowel string

---
+++ src/com/java/streams/Vowels.java
@@ -0,0 +1,19 @@
+package com.java.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//return the list which contain vowels
+public class Vowels {
+	public static void main(String args[]) {
+		List<String> list = Arrays.asList("Indian", "Arizona", "Fly", "mrp", "prp");
+		List<String> newList = list.stream().filter(ele -> {
+			List<Character> characters = Arrays.asList('a', 'e', 'i', 'o', 'u');
+			return ele.chars().mapToObj(c -> (char) c).anyMatch(characters::contains);
+		}).collect(Collectors.toList());
+
+		newList.forEach(System.out::println);
+
+	}
+}
