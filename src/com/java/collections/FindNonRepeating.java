0f3e9e51af942584967ff32ebce2a632bdd367d0
HarishGanji <harishganji7989@gmail.com>
2025-01-18 13:11:49 +0530

    Non-Repeating Number

--- src/com/java/collections/FindNonRepeating.java
+++ src/com/java/collections/FindNonRepeating.java
@@ -1,12 +1,23 @@
 package com.java.collections;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FindNonRepeating {
 	public static void main(String args[]) {
-		List<Integer> list = new ArrayList<>(Arrays.asList(1,1,2,2,3,3,5));
-		
+		List<Integer> elements = Arrays.asList(4, 5, 1, 2, 3, 1, 5, 2, 6);
+		Map<Integer, Integer> nonRepeatingTracking = new LinkedHashMap<>();
+		for (int num : elements) {
+			if (nonRepeatingTracking.containsKey(num)) {
+				int currentCount = nonRepeatingTracking.get(num);
+				nonRepeatingTracking.put(num, currentCount + 1);
+			} else {
+				nonRepeatingTracking.put(num, 1);
+			}
+		}
+		int num = nonRepeatingTracking.entrySet().stream().findFirst().get().getKey();
+		System.out.println("Non-Repeating number is: " + num);
 	}
 }
